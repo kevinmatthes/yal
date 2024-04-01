5430610f7de391128106008380c6f85d93c9b618
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2024-04-01 17:27:33 +0200

    [Enhancement] Setup core library (#1)
    * Added ::= .gitignore
    
    * Added ::= core/pom.xml
    
    * Skip ::= register formatting phase as compilation step
    
    * Added ::= core/src/main/java/io/github/kevinmatthes/yal/App.java
    
    * Skip ::= set version; add code execution plugin
    
    * Added ::= src/main/java/io/github/kevinmatthes/yal/Yal.java
    
    * Skip ::= Rename App.java to Yal.java
    
    * Skip ::= configure plugin
    
    * Create summary of recent changes

---
+++ core/src/main/java/io/github/kevinmatthes/yal/Yal.java
@@ -0,0 +1,66 @@
+/*********************** GNU General Public License 3.0 ***********************\
+|                                                                              |
+|  Copyright (C) 2024 Kevin Matthes                                            |
+|                                                                              |
+|  This program is free software: you can redistribute it and/or modify        |
+|  it under the terms of the GNU General Public License as published by        |
+|  the Free Software Foundation, either version 3 of the License, or           |
+|  (at your option) any later version.                                         |
+|                                                                              |
+|  This program is distributed in the hope that it will be useful,             |
+|  but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+|  GNU General Public License for more details.                                |
+|                                                                              |
+|  You should have received a copy of the GNU General Public License           |
+|  along with this program.  If not, see <https://www.gnu.org/licenses/>.      |
+|                                                                              |
+\******************************************************************************/
+
+package io.github.kevinmatthes.yal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * The main class.
+ *
+ * @author Kevin Matthes
+ *
+ * @version 0.1.0
+ */
+public class Yal {
+    /**
+     * The main method.
+     *
+     * @author Kevin Matthes
+     *
+     * @param args
+     *            The command line arguments.
+     *
+     * @version 0.1.0
+     */
+    public static void main(String[] args) throws IOException {
+        repl();
+    }
+
+    private static void repl() throws IOException {
+        InputStreamReader i = new InputStreamReader(System.in);
+        BufferedReader r = new BufferedReader(i);
+
+        for (;;) {
+            System.out.print("> ");
+            final String l = r.readLine();
+
+            if (l == null)
+                break;
+            else
+                run(l);
+        }
+    }
+
+    private static void run(String code) {
+        System.out.println(code);
+    }
+}
