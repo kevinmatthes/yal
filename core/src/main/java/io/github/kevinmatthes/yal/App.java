7b08307605020412e3cfa6439a4f15e72fd54e48
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2024-04-01 16:00:16 +0200

    Added ::= core/src/main/java/io/github/kevinmatthes/yal/App.java

---
+++ core/src/main/java/io/github/kevinmatthes/yal/App.java
@@ -0,0 +1,43 @@
+/*********************** GNU General Public License 3.0 ***********************\
+|                                                                              |
+|  Copyright (C) 2024 Kevin Matthes                                            |
+|                                                                              |
+|  This program is free software: you can redistribute it and/or modify        |
+|  it under the terms of the GNU General Public License as published by        |
+|  the Free Software Foundation, either version 3 of the License, or           |
+|  (at your option) any later version.                                         |
+|                                                                              |
+|  This program is distributed in the hope that it will be useful,             |
+|  but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+|  GNU General Public License for more details.                                |
+|                                                                              |
+|  You should have received a copy of the GNU General Public License           |
+|  along with this program.  If not, see <https://www.gnu.org/licenses/>.      |
+|                                                                              |
+\******************************************************************************/
+
+package io.github.kevinmatthes.yal;
+
+/**
+ * The main class.
+ *
+ * @author Kevin Matthes
+ *
+ * @version 0.1.0
+ */
+public class App {
+    /**
+     * The main method.
+     *
+     * @author Kevin Matthes
+     *
+     * @param args
+     *            The command line arguments.
+     *
+     * @version 0.1.0
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
